77efc65385a1f8106c26985d92d388de06b19481
oopall <opalamsalem@hilma.tech>
2023-11-13 15:51:32 +0200

    grid

--- GetTo100/src/components/Board.jsx
+++ GetTo100/src/components/Board.jsx
@@ -1,5 +1,17 @@
+import "../css/Board.css";
+import Game from "./Game";
+import ScoreBoard from "./ScoreBoard";
+
 function Board() {
-    return (  );
+  return (
+    <div id="board-grid">
+      <Game id="game1" />
+      <Game id="game2" />
+      <Game id="game3" />
+      <Game id="game4" />
+      <ScoreBoard id="scores" />
+    </div>
+  );
 }
 
 export default Board;
