cc69a4ad83fc5030bb1c2f4b9c6ca7d400ea99bb
oopall <opalamsalem@hilma.tech>
2023-11-14 12:12:11 +0200

    score board almost working

--- GetTo100/src/components/ScoreBoard.jsx
+++ GetTo100/src/components/ScoreBoard.jsx
@@ -6,43 +6,81 @@ function ScoreBoard(props) {
     });
     return count;
   }
+
   function usersWithoutNull(users) {
     const onlyUsers = users.filter((user) => user !== null);
     return onlyUsers;
   }
-  function findUserWithMostMoves(users) {
-    const playerWithMostWins = usersWithoutNull(users).reduce(
-      (prev, current) => {
-        return prev.moves > current.moves ? prev : current;
-      }
+
+  function checkTie(users, moves) {
+    const userWithSameScore = usersWithoutNull(users).filter(
+      (user) => user.moves === moves
     );
-    return playerWithMostWins;
-  }
-  function findUserWithLeastMoves(users) {
-    const playerWithMostWins = usersWithoutNull(users).reduce(
-      (prev, current) => {
-        console.log("p " + prev);
-        console.log("c " + current);
-        return prev.moves < current.moves ? prev : current;
-      }
-    );
-    return playerWithMostWins;
+    return userWithSameScore;
   }
 
-  console.log(props.playingUsers);
+  //   function findUserWithMostMoves(users) {
+  //     const playerWithMostWins = usersWithoutNull(users).reduce(
+  //       (prev, current) => {
+  //         return prev.moves > current.moves ? prev : current;
+  //       }
+  //     );
+  //     return playerWithMostWins;
+  //   }
+
+  function usersWithLeastMoves(users) {
+    const movesArr = usersWithoutNull(users).map((user) => user.moves);
+    const min = Math.min(...movesArr);
+    const usersWithLeastArr = checkTie(usersWithoutNull(users), min);
+    return usersWithLeastArr;
+  }
+
+  function usersWithMostMoves(users) {
+    const movesArr = usersWithoutNull(users).map((user) => user.moves);
+    console.log(movesArr);
+    const max = Math.max(...movesArr);
+    const usersWithMostArr = checkTie(usersWithoutNull(users), max);
+    return usersWithMostArr;
+  }
+
+  function strUsernames(users) {
+    let str = "";
+    for (let i = 0; i < users.length; i++) {
+      str += users[i].username;
+      if (i !== users.length - 1) {
+        str += " & ";
+      }
+    }
+    return str;
+  }
+
+  //   function findUserWithLeastMoves(users) {
+  //     const playerWithMostWins = usersWithoutNull(users).reduce(
+  //       (prev, current) => {
+  //         console.log("p " + prev);
+  //         console.log("c " + current);
+  //         return prev.moves < current.moves ? prev : current;
+  //       }
+  //     );
+  //     return playerWithMostWins;
+  //   }
+
   return (
     <div id="scores">
       {props.playingUsers.findIndex((user) => user !== null) >= 0 ? (
         findPlayers(props.playingUsers) === 1 ? (
           <h3>You are the only player right now</h3>
+        ) : strUsernames(usersWithLeastMoves(props.playingUsers)) ===
+          strUsernames(usersWithMostMoves(props.playingUsers)) ? (
+          <h3>There is a tie in moves</h3>
         ) : (
           <div>
             <h4>
-              {findUserWithMostMoves(props.playingUsers).username} did the most
-              moves
+              {strUsernames(usersWithMostMoves(props.playingUsers))} did the
+              most moves
             </h4>
             <h4>
-              {findUserWithLeastMoves(props.playingUsers).username} did the
+              {strUsernames(usersWithLeastMoves(props.playingUsers))} did the
               least moves
             </h4>
           </div>
