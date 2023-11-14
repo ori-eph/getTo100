666e484e5136fb7fb86ebbc8f30c723d5b95a446
ori-eph <oriephrati@hilma.tech>
2023-11-14 14:09:02 +0200

    orgenized code

--- GetTo100/src/components/GameBtn.jsx
+++ GetTo100/src/components/GameBtn.jsx
@@ -1,109 +1,128 @@
 function GameBtn(props) {
+  function plus(number) {
+    props.setUser((prev) => {
+      return {
+        ...prev,
+        currentNumber: Math.floor(prev.currentNumber + number),
+        moves: prev.moves + 1,
+      };
+    });
+    props.setPlayingUsers((prev) => {
+      return [
+        ...prev.slice(0, props.index),
+        { ...props.user, moves: prev[props.index].moves + 1 },
+        ...prev.slice(props.index + 1),
+      ];
+    });
+    if (props.user.currentNumber + 1 === 100) {
+      props.setUser((prev) => {
+        return {
+          ...prev,
+          scores: [...prev.scores, prev.moves],
+        };
+      });
+      props.setDidWin(true);
+    }
+    props.setTurn();
+  }
+
+  function minus(number) {
+    props.setUser((prev) => {
+      return {
+        ...prev,
+        currentNumber: Math.floor(prev.currentNumber - number),
+        moves: prev.moves + 1,
+      };
+    });
+    props.setPlayingUsers((prev) => {
+      return [
+        ...prev.slice(0, props.index),
+        { ...props.user, moves: prev[props.index].moves + 1 },
+        ...prev.slice(props.index + 1),
+      ];
+    });
+    if (props.user.currentNumber - 1 === 100) {
+      props.setUser((prev) => {
+        return {
+          ...prev,
+          scores: [...prev.scores, prev.moves],
+        };
+      });
+      props.setDidWin(true);
+    }
+    props.setTurn();
+  }
+
+  function divide(number) {
+    props.setPlayingUsers((prev) => {
+      return [
+        ...prev.slice(0, props.index),
+        { ...props.user, moves: prev[props.index].moves + 1 },
+        ...prev.slice(props.index + 1),
+      ];
+    });
+    props.setUser((prev) => {
+      return {
+        ...prev,
+        currentNumber: Math.floor(prev.currentNumber / number),
+        moves: prev.moves + 1,
+      };
+    });
+    if (props.user.currentNumber / 2 === 100) {
+      props.setUser((prev) => {
+        return {
+          ...prev,
+          scores: [...prev.scores, prev.moves],
+        };
+      });
+      props.setDidWin(true);
+    }
+    props.setTurn();
+  }
+
+  function multiply(number) {
+    props.setUser((prev) => {
+      return {
+        ...prev,
+        currentNumber: Math.floor(prev.currentNumber * number),
+        moves: prev.moves + 1,
+      };
+    });
+    props.setPlayingUsers((prev) => {
+      return [
+        ...prev.slice(0, props.index),
+        { ...props.user, moves: prev[props.index].moves + 1 },
+        ...prev.slice(props.index + 1),
+      ];
+    });
+    if (props.user.currentNumber * 2 === 100) {
+      props.setUser((prev) => {
+        return {
+          ...prev,
+          scores: [...prev.scores, prev.moves],
+        };
+      });
+      props.setDidWin(true);
+    }
+    props.setTurn();
+  }
+
   function doAction(operator, number) {
     switch (operator) {
       case "/":
-        props.setPlayingUsers((prev) => {
-          return [
-            ...prev.slice(0, props.index),
-            { ...props.user, moves: prev[props.index].moves + 1 },
-            ...prev.slice(props.index + 1),
-          ];
-        });
-        props.setUser((prev) => {
-          return {
-            ...prev,
-            currentNumber: Math.floor(prev.currentNumber / number),
-            moves: prev.moves + 1,
-          };
-        });
-        if (props.user.currentNumber / 2 === 100) {
-          props.setUser((prev) => {
-            return {
-              ...prev,
-              scores: [...prev.scores, prev.moves],
-            };
-          });
-          props.setDidWin(true);
-        }
-        props.setTurn();
+        divide(number);
         break;
+
       case "*":
-        props.setUser((prev) => {
-          return {
-            ...prev,
-            currentNumber: Math.floor(prev.currentNumber * number),
-            moves: prev.moves + 1,
-          };
-        });
-        props.setPlayingUsers((prev) => {
-          return [
-            ...prev.slice(0, props.index),
-            { ...props.user, moves: prev[props.index].moves + 1 },
-            ...prev.slice(props.index + 1),
-          ];
-        });
-        if (props.user.currentNumber * 2 === 100) {
-          props.setUser((prev) => {
-            return {
-              ...prev,
-              scores: [...prev.scores, prev.moves],
-            };
-          });
-          props.setDidWin(true);
-        }
-        props.setTurn();
+        multiply(number);
         break;
+
       case "+":
-        props.setUser((prev) => {
-          return {
-            ...prev,
-            currentNumber: Math.floor(prev.currentNumber + number),
-            moves: prev.moves + 1,
-          };
-        });
-        props.setPlayingUsers((prev) => {
-          return [
-            ...prev.slice(0, props.index),
-            { ...props.user, moves: prev[props.index].moves + 1 },
-            ...prev.slice(props.index + 1),
-          ];
-        });
-        if (props.user.currentNumber + 1 === 100) {
-          props.setUser((prev) => {
-            return {
-              ...prev,
-              scores: [...prev.scores, prev.moves],
-            };
-          });
-          props.setDidWin(true);
-        }
-        props.setTurn();
+        plus(number);
         break;
+
       case "-":
-        props.setUser((prev) => {
-          return {
-            ...prev,
-            currentNumber: Math.floor(prev.currentNumber - number),
-            moves: prev.moves + 1,
-          };
-        });
-        props.setPlayingUsers((prev) => {
-          return [
-            ...prev.slice(0, props.index),
-            { ...props.user, moves: prev[props.index].moves + 1 },
-            ...prev.slice(props.index + 1),
-          ];
-        });
-        if (props.user.currentNumber - 1 === 100) {
-          props.setUser((prev) => {
-            return {
-              ...prev,
-              scores: [...prev.scores, prev.moves],
-            };
-          });
-          props.setDidWin(true);
-        }
-        props.setTurn();
+        minus(number);
         break;
     }
   }
